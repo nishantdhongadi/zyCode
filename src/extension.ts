71b31b14c4d6f02568d023aa7ede5e03f8baad55
nishantdhongadi <nishantdhongadi@gmail.com>
2024-02-12 23:51:19 -0700

    Removed redundant and duplicated files. Modified extension file to listen for file save events.
    Changes to be committed:
        modified:   src/extension.ts
        modified:   src/test/extension.test.ts
        modified:   tsconfig.json
        deleted:    zyCode/.eslintrc.json
        deleted:    zyCode/.vscode-test.mjs
        deleted:    zyCode/.vscode/extensions.json
        deleted:    zyCode/.vscode/launch.json
        deleted:    zyCode/.vscode/settings.json
        deleted:    zyCode/.vscode/tasks.json
        deleted:    zyCode/.vscodeignore
        deleted:    zyCode/CHANGELOG.md
        deleted:    zyCode/README.md
        deleted:    zyCode/package-lock.json
        deleted:    zyCode/package.json
        deleted:    zyCode/src/extension.js
        deleted:    zyCode/src/extension.js.map
        deleted:    zyCode/src/extension.ts
        deleted:    zyCode/src/test/extension.test.js
        deleted:    zyCode/src/test/extension.test.js.map
        deleted:    zyCode/src/test/extension.test.ts
        deleted:    zyCode/tsconfig.json
        deleted:    zyCode/vsc-extension-quickstart.md

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,12 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
+        vscode.window.showInformationMessage(`File saved: ${document.fileName}`);
+        // Here, you would add the code to update the zyBooks code editor.
+    });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "zyCode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('zyCode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from zyCode!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,11 +5,8 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
-
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+describe('Extension Test Suite', () => {
+    test('Sample Test', () => {
+        expect(true).toBe(true);
+    });
 });
